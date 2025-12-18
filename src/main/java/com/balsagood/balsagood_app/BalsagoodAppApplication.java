9eebee227490699189b99cfc6045b235fc48db83
CarlosFPG <carlos.patinogarcia9447@upse.edu.ec>
2025-12-18 14:20:27 -0500

    nuevo proyecto balsagood-app

---
+++ src/main/java/com/balsagood/balsagood_app/BalsagoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.balsagood.balsagood_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalsagoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalsagoodAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balsagood/balsagood_app/BalsagoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balsagood.balsagood_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalsagoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
